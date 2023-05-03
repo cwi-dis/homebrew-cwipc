091d355527523802c69a1f0d297b08b3b21a42c4
CWI DIS Group <cwidisgroup@gmail.com>
2023-05-03 13:39:10 +0200

    Updated for 7.4

--- cwipc.rb
+++ cwipc.rb
@@ -5,14 +5,14 @@ class Cwipc < Formula
   desc "CWI point cloud software suite"
   homepage "https://github.com/cwi-dis/cwipc"
   license "MIT"
-  url "https://github.com/cwi-dis/cwipc/releases/download/v7.3.7/cwipc-v7.3.7-source-including-submodules.tar.gz"
-  sha256 "7338128971783c89227e42ed089afe184692adfa2995ea7f546dcc6146fe2ff2"
-  version "7.3.7"
+  url "https://github.com/cwi-dis/cwipc/releases/download/v7.4/cwipc-v7.4-source-including-submodules.tar.gz"
+  sha256 "bcb50a9cda62b6f9fc940453d799636d22101d62ff97712b29922ea81122815e"
+  version "7.4"
 
   depends_on "cmake" => :build
   depends_on "git-lfs" => :build
   depends_on "pcl"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "jpeg-turbo"
   depends_on "librealsense" => :recommended
 
@@ -23,7 +23,7 @@ class Cwipc < Formula
     # This is not really okay: it will install the scripts into /usr/local/bin (or /opt equivalent) while they should be in the cellar.
     # Have opened https://github.com/pypa/pip/issues/11253 to see if there is a workaround.
     #system "#{bin}/cwipc_pymodules_install.sh"
-    #system Formula["python@3.9"].opt_bin/"pip3", "--verbose", "install", "--upgrade", "--find-links", "#{prefix}/share/cwipc/python", "cwipc_util", "cwipc_codec", "cwipc_realsense2"
+    #system Formula["python@3.10"].opt_bin/"pip3", "--verbose", "install", "--upgrade", "--find-links", "#{prefix}/share/cwipc/python", "cwipc_util", "cwipc_codec", "cwipc_realsense2"
     opoo "Please run cwipc_pymodules_install.sh manually to install the Python-based cwipc command line tools"
   end
 
