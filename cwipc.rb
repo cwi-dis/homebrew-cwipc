89c713f068b3889378d99038b9417714bacdab68
Jack Jansen <Jack.Jansen@cwi.nl>
2024-03-26 18:45:58 +0100

    Updated for v7.5.0 release

--- cwipc.rb
+++ cwipc.rb
@@ -1,38 +1,36 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Cwipc < Formula
   desc "CWI point cloud software suite"
   homepage "https://github.com/cwi-dis/cwipc"
+  url "https://github.com/cwi-dis/cwipc/releases/download/v7.5.0/cwipc-v7.5.0-source-including-submodules.tar.gz"
+  version "7.5.0"
   license "MIT"
-  url "https://github.com/cwi-dis/cwipc/releases/download/v7.4.2/cwipc-v7.4.2-source-including-submodules.tar.gz"
-  sha256 "bdeac677569cb71c91e247e1182cb297fe7d86cdbf845caa00da8f68f42544e5"
-  head "https://github.com/cwi-dis/cwipc.git"
-  # version "7.4.1"
-
+  sha256 "79bff2884403c8672552a8ee17869ed226d13b358eff6c65a595ce2471afdebb"
+  head "https://github.com/cwi-dis/cwipc.git#master"
+  
   depends_on "cmake" => :build
   depends_on "git-lfs" => :build
-  depends_on "pcl"
-  depends_on "python@3.10"
   depends_on "jpeg-turbo"
+  depends_on "pcl"
+  depends_on "python@3.11"
   depends_on "librealsense" => :recommended
 
   def install
-    pyFormula = Formula["python@3.10"]
+    py_formula = Formula["python@3.11"]
     if build.head?
       # Use normal version-finding scheme
-      system "cmake", "-S", ".", "-B", "build", "-DPython3_ROOT_DIR=#{pyFormula.opt_prefix}", "-DCWIPC_SKIP_PYTHON_INSTALL=1", *std_cmake_args
+      system "cmake", "-S", ".", "-B", "build", "-DPython3_ROOT_DIR=#{py_formula.opt_prefix}", "-DCWIPC_SKIP_PYTHON_INSTALL=1", *std_cmake_args
     else
-      system "cmake", "-S", ".", "-B", "build", "-DPython3_ROOT_DIR=#{pyFormula.opt_prefix}", "-DCWIPC_VERSION=#{version}", "-DCWIPC_SKIP_PYTHON_INSTALL=1", *std_cmake_args
+      system "cmake", "-S", ".", "-B", "build", "-DPython3_ROOT_DIR=#{py_formula.opt_prefix}", "-DCWIPC_VERSION=#{version}", "-DCWIPC_SKIP_PYTHON_INSTALL=1", *std_cmake_args
     end
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     # Install a link cwipc_python that points to the Python used to install
-    ln_sf pyFormula.opt_bin/"python3.10", "#{prefix}/bin/cwipc_python"
+    ln_sf py_formula.opt_bin/"python3.11", "#{bin}/cwipc_python"
     # Hack to make cwipc prefix directory a valid destination for pip installs
-    system "mkdir", "-p", "#{prefix}/lib/python3.10/site-packages"
-    system pyFormula.opt_bin/"python3.10", "-m", "pip", "--verbose", "install", "--prefix", prefix, "--upgrade", "--find-links", "#{prefix}/share/cwipc/python", "cwipc_util", "cwipc_codec", "cwipc_realsense2"
-
+    #system "mkdir", "-p", "#{prefix}/lib/python3.11"
+    #system "mkdir", "-p", "#{prefix}/lib/python3.11/site-packages"
+    #system "ls", "-lRa", "#{prefix}/"
+    system "#{bin}/cwipc_python", "-m", "pip", "--verbose", "install", "--prefix", prefix, "--upgrade", "--find-links", "#{pkgshare}/python", "cwipc_util", "cwipc_codec", "cwipc_realsense2"
   end
 
   test do
