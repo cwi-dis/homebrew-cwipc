d8e58c71fefe420724e620544e428e2083736a66
Jack Jansen <Jack.Jansen@cwi.nl>
2025-01-19 02:16:12 +0100

    Preparing for 7.6 release

--- cwipc.rb
+++ cwipc.rb
@@ -1,10 +1,10 @@
 class Cwipc < Formula
   desc "CWI point cloud software suite"
   homepage "https://github.com/cwi-dis/cwipc"
-  url "https://github.com/cwi-dis/cwipc/releases/download/v7.5.3/cwipc-v7.5.3-source-including-submodules.tar.gz"
-  version "7.5.3"
+  url "https://github.com/cwi-dis/cwipc/releases/download/v7.6.0/cwipc-v7.6.0-source-including-submodules.tar.gz"
+  version "7.6.0"
   license "MIT"
-  sha256 "d24d5b0b95b926c2f28e90f01bd1b31e09116c06163d6693232c934999fdcd0d"
+  sha256 "0"
   head "https://github.com/cwi-dis/cwipc.git", branch: "master"
   
   depends_on "cmake" => :build
@@ -12,11 +12,11 @@ class Cwipc < Formula
   depends_on "nlohmann-json" => :build
   depends_on "jpeg-turbo"
   depends_on "pcl"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
   depends_on "librealsense" => :recommended
 
   def install
-    py_formula = Formula["python@3.11"]
+    py_formula = Formula["python@3.12"]
     if build.head?
       # Use normal version-finding scheme
       system "cmake", "-S", ".", "-B", "build", "-DPython3_ROOT_DIR=#{py_formula.opt_prefix}", "-DCWIPC_SKIP_PYTHON_INSTALL=1", *std_cmake_args
@@ -26,10 +26,10 @@ class Cwipc < Formula
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     # Install a link cwipc_python that points to the Python used to install
-    ln_sf py_formula.opt_bin/"python3.11", "#{bin}/cwipc_python"
+    ln_sf py_formula.opt_bin/"python3.12", "#{bin}/cwipc_python"
     # Hack to make cwipc prefix directory a valid destination for pip installs
-    #system "mkdir", "-p", "#{prefix}/lib/python3.11"
-    #system "mkdir", "-p", "#{prefix}/lib/python3.11/site-packages"
+    #system "mkdir", "-p", "#{prefix}/lib/python3.12"
+    #system "mkdir", "-p", "#{prefix}/lib/python3.12/site-packages"
     #system "ls", "-lRa", "#{prefix}/"
     system "#{bin}/cwipc_python", "-m", "pip", "--verbose", "install", "--prefix", prefix, "--upgrade", "--find-links", "#{pkgshare}/python", "cwipc_util", "cwipc_codec", "cwipc_realsense2"
   end
